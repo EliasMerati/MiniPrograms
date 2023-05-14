c59a55bfd3e87377aa909b9ba2aa42112008f274
elias.merati <elias.merati@gmail.com>
2023-05-14 23:44:08 +0430

    work with reflections

---
+++ WorkingWithReflections/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using static System.Console;
+
+WriteLine("Assembly MetaData:");
+Assembly assembly = Assembly.GetEntryAssembly();
+WriteLine($"FullName : {assembly.FullName}");
+WriteLine($"Location : {assembly.Location}");
+
+var attributes = assembly.GetCustomAttributes();
+WriteLine($"Attributes :");
+foreach (Attribute a in attributes)
+{
+    WriteLine($"{a.GetType()}");
+}
+///////////////////////////////////////////////////////////////////////// Information
+WriteLine("---------------------------------------------------------------------");
+var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+WriteLine($" Version : {version.InformationalVersion}");
+
+var company = assembly.GetCustomAttribute<AssemblyCompanyAttribute>();
+WriteLine($"Company : {company.Company}");
+ReadKey();
