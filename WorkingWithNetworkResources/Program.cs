780f50006d0f5edcf766d419ff22f1003c537912
elias.merati <elias.merati@gmail.com>
2023-05-14 22:06:57 +0430

    Add Ping Status

--- WorkingWithNetworkResources/Program.cs
+++ WorkingWithNetworkResources/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Net.NetworkInformation;
 using static System.Console;
 
 Write("Enter A Valid Web Address: ");
@@ -18,11 +19,29 @@ WriteLine($"Uri OriginalString : {Uri.OriginalString}");
 WriteLine($"Uri Scaped : {Uri.UserEscaped}");
 
 ////////////////////// Get Ip
+
 IPHostEntry entry = Dns.GetHostEntry(Uri.Host);
 WriteLine($"{entry.HostName} Has The Folowing Ip Addresses :");
 foreach (IPAddress address in entry.AddressList)
 {
     Write($"{address}");
+}
+
+///////////////////////////////////////// ping site
+try
+{
+    var ping = new Ping();
+    WriteLine("Pinging Server . Please Wait ....");
+    PingReply Reply = ping.Send(Uri.Host);
+    WriteLine($"{Uri.Host} Was Pinged and Replied {Reply.Status}.");
+    if (Reply.Status is IPStatus.Success)
+    {
+        WriteLine($"Reply From {Reply.Address} Took {Reply.RoundtripTime:N0}ms");
+    }
+}
+catch (Exception ex)
+{
+   WriteLine($"{ex.GetType()} says {ex.Message}");
 }
 
 ReadKey();
